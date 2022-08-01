f43f613324c3a52a2dd89b25a3be66e81f19c0ee
Josephine Leung <josephine.shunhay@gmail.com>
2022-07-31 17:39:07 -0700

    cleaned up notes section

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,35 +46,36 @@ const Home: NextPage = () => {
             <p>
               <b>Getting Started with Next.js</b>
               <br />
-              1. I wouldn&rsquo;t have set up my terminal to accept this command
-              (make sure you have a Node env on your computer). Don&rsquo;t have
-              Node installed locally, so have to install it first.
+              1. I didn&rsquo;t  set up my terminal to accept this command. Having a message like "You need to have a Node env on your computer" would have been helpful. Sine I don&rsquo;t have
+              Node installed locally, I have to install it first.
               <br />
-              2. Before installing Node, have to install Xcode command line
-              tools
+              2. Before installing Node, I have to install Xcode command line
+              tools.
               <br />
-              3. After installing Node.js, input command. Okay to install
-              package?
+              3. After installing Node.js, I&rsquo;m asked to input command. The directions don&rsquo;t mention this so I&rsquo;m unsure if it&rsquo;s okay to install the
+              package.
               <br />
-              4. Installed node is too new. I need to install an older version
+              4. The I installed is too new. I need to install an older version.
               <br />
-              5. After install, suggest &ldquo;ls&rdquo; to make sure the
-              platforms folder has been created
+              5. After installation, I would add a suggestion for the user to type &ldquo;ls&rdquo; to make sure the
+              platforms folder has been created.
               <br />
-              6. Note: Don&apos;t forget to convert the .env.example file
+              6. The instructions at the bottom incude <i>Note: Don&apos;t forget to convert the .env.example file
               that&rsquo;s located at the root of the repo into a .env file
-              &ndash;&nbsp;it&apos;ll come in handy later. How would I do that?
+              &ndash;&nbsp;it&apos;ll come in handy later.</i> How would I do that?
               <br />
               <br />
               <b>Using a Template</b>
-              <br />- Next.js documentation much easier because it uses the
-              platform.
-              <br />- Ran into too many errors from the available examples from
-              the Vercel examples repo
-              <br />- Decided to start from totally new Next.js project
-              <br />- Deployed with no problem
-              <br />- Good milestone message
-              <br />- Good preview
+              <br />- Using the examples in the Next.js documentation is much easier, since it uses a 1-click deployment.
+              <br />- I ran into too many errors from the available examples from
+              the Next.js examples repo.
+              <br />- I decided to start from totally new Next.js project.
+              <br />- I was able to deploy with no problem.
+              <br />- Good milestone message.
+              <br />- Good preview.
+              <br />- I edited my site in VS code.
+              <br />- I used my terminal to push my changes to github.
+
             </p>
           </a>
         </div>

4d351c4838b44407b2885bbf3406f5ed5fc6ab8a
Josephine Leung <josephine.shunhay@gmail.com>
2022-07-31 17:43:33 -0700

    Escape double quotes

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,14 +8,21 @@ const Home: NextPage = () => {
     <div className={styles.container}>
       <Head>
         <title>Josephine Leung</title>
-        <meta name="description" content="I&apos;m a product manager in the San Francisco Bay Area" />
+        <meta
+          name="description"
+          content="I'm a product manager in the San Francisco Bay Area"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
           Hi, I&apos;m{" "}
-          <a href="https://www.linkedin.com/in/jforjosephine/" target="blank" rel="noopener noreferrer">
+          <a
+            href="https://www.linkedin.com/in/jforjosephine/"
+            target="blank"
+            rel="noopener noreferrer"
+          >
             Josephine Leung!
           </a>
         </h1>
@@ -24,7 +31,8 @@ const Home: NextPage = () => {
           <a
             href="https://www.ravelry.com/people/jforjosephine/queue"
             className={styles.card}
-            target="_blank" rel="noopener noreferrer"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             <h2>Knitting &rarr;</h2>
             <p>View my WIP, yarn stash, and knitting queue on Ravelry.</p>
@@ -33,7 +41,8 @@ const Home: NextPage = () => {
           <a
             href="https://letterboxd.com/jforjosephine/"
             className={styles.card}
-            target="_blank" rel="noopener noreferrer"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             <h2>Movies &rarr;</h2>
             <p>I keep my movie-watching diary and wishlist on Letterboxd.</p>
@@ -41,41 +50,53 @@ const Home: NextPage = () => {
         </div>
 
         <div className={styles.notescard}>
-          <a href="https://github.com/josephineleung/blog" target="blank" rel="noopener noreferrer">
+          <a
+            href="https://github.com/josephineleung/blog"
+            target="blank"
+            rel="noopener noreferrer"
+          >
             <h2>Notes &rarr;</h2>
             <p>
               <b>Getting Started with Next.js</b>
               <br />
-              1. I didn&rsquo;t  set up my terminal to accept this command. Having a message like "You need to have a Node env on your computer" would have been helpful. Sine I don&rsquo;t have
-              Node installed locally, I have to install it first.
+              1. I didn&rsquo;t set up my terminal to accept this command.
+              Having a message like &ldquo;You need to have a Node env on your
+              computer&rdquo; would have been helpful. Sine I don&rsquo;t have Node
+              installed locally, I have to install it first.
               <br />
               2. Before installing Node, I have to install Xcode command line
               tools.
               <br />
-              3. After installing Node.js, I&rsquo;m asked to input command. The directions don&rsquo;t mention this so I&rsquo;m unsure if it&rsquo;s okay to install the
-              package.
+              3. After installing Node.js, I&rsquo;m asked to input command. The
+              directions don&rsquo;t mention this so I&rsquo;m unsure if
+              it&rsquo;s okay to install the package.
               <br />
               4. The I installed is too new. I need to install an older version.
               <br />
-              5. After installation, I would add a suggestion for the user to type &ldquo;ls&rdquo; to make sure the
-              platforms folder has been created.
+              5. After installation, I would add a suggestion for the user to
+              type &ldquo;ls&rdquo; to make sure the platforms folder has been
+              created.
               <br />
-              6. The instructions at the bottom incude <i>Note: Don&apos;t forget to convert the .env.example file
-              that&rsquo;s located at the root of the repo into a .env file
-              &ndash;&nbsp;it&apos;ll come in handy later.</i> How would I do that?
+              6. The instructions at the bottom incude{" "}
+              <i>
+                Note: Don&apos;t forget to convert the .env.example file
+                that&rsquo;s located at the root of the repo into a .env file
+                &ndash;&nbsp;it&apos;ll come in handy later.
+              </i>{" "}
+              How would I do that?
               <br />
               <br />
               <b>Using a Template</b>
-              <br />- Using the examples in the Next.js documentation is much easier, since it uses a 1-click deployment.
-              <br />- I ran into too many errors from the available examples from
-              the Next.js examples repo.
+              <br />- Using the examples in the Next.js documentation is much
+              easier, since it uses a 1-click deployment.
+              <br />- I ran into too many errors from the available examples
+              from the Next.js examples repo.
               <br />- I decided to start from totally new Next.js project.
               <br />- I was able to deploy with no problem.
               <br />- Good milestone message.
               <br />- Good preview.
               <br />- I edited my site in VS code.
               <br />- I used my terminal to push my changes to github.
-
             </p>
           </a>
         </div>
